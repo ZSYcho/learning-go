c49d0604c1d312caf33d17143ea38616e9af538e
kevin <2496919420@qq.com>
2026-07-07 07:54:25 +0800

    Add channles snippet

---
+++ 9section/3-channels/main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+type user struct {
+	name string
+}
+
+func main() {
+
+	messages := make(chan string)
+
+	users := make(chan user)
+
+	go func() {
+		fmt.Println("Sending a message to messages channel")
+		messages <- "Hello from messages channel"
+	}()
+
+	go func() {
+		fmt.Println("Sending a message to messages channel")
+		messages <- "Hello from messages channel"
+	}()
+
+	go func() {
+		fmt.Println("Sending a message to users channel")
+		users <- user{name: "John"}
+	}()
+
+	time.Sleep(1 * time.Second)
+
+	fmt.Println("About to get message from channel")
+	msg := <-messages
+	fmt.Println(msg)
+
+	msg = <-messages
+	fmt.Println(msg)
+
+	u := <-users
+	fmt.Println(u)
+}
