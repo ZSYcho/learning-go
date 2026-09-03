483bebf64a327c38104848fd415e4900a3492cc8
kevin <2496919420@qq.com>
2026-09-03 18:01:19 +0800

    Add session management

--- 13web/handlers.go
+++ 13web/handlers.go
@@ -7,12 +7,14 @@ import (
 // we have three handlers
 
 func (app *application) home(w http.ResponseWriter, r *http.Request) {
+	app.infoLog.Printf("Session data: %s", app.session.GetString(r, "userID"))
 	app.render(w, "index.html", nil)
 	//homeContent := fmt.Sprintf(htmlContent, "Home", "<h1>Hello, welcome to the homepage</h1>")
 	//_, _ = w.Write([]byte(homeContent))
 }
 
 func (app *application) login(w http.ResponseWriter, r *http.Request) {
+	app.session.Put(r, "userID", "kevin")
 	app.render(w, "login.html", nil)
 }
 
